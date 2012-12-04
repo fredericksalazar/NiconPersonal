1d02e7196301740d5666b49409d12a11f96e3c4e
frederick <frederick@frederick-Aspire-5536>
2012-12-03 23:16:49 -0500

    Se termina de documentar NiconSystemAdmin se hacen algunos cambios en los nombres de algunos metodos, de igual forma se mejoran y pulen metodos esenciales del sistema

--- src/org/Nicon/Personal/LibCore/Sbin/NiconSystemAdmin.java
+++ src/org/Nicon/Personal/LibCore/Sbin/NiconSystemAdmin.java
@@ -23,20 +23,24 @@ public class NiconSystemAdmin {
 
     private static final char[] HEXADECIMAL = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b',
         'c', 'd', 'e', 'f'};
+    
     private static boolean StateActivationSystem = false;
     private static boolean stateOP;
-    private static String EncoderCodification;
+    
     private static ResultSet Data;
     private static SimpleDateFormat format;
     private static Date date;
-    private static StringBuilder sb;
-    private static String sentence;
+    
     private static int WidthScreen;
     private static int HeightScreen;
     private static NiconAdministratorDAO niconadminDAO;
     private static NiconAdministratorDAO niconadmin;
-
     
+    private static String EncoderCodification;
+    private static String dateFormated;
+    private static String sentence;
+    private static StringBuilder sb;
+        
     
     /**
      * Este metodo estatico provee una herramienta de validacion de estado del sistema, hace uso de 
@@ -65,7 +69,7 @@ public class NiconSystemAdmin {
     public static void activeNiconPersonal(NiconAdministrator administrator) {
 
         try {
-            String password = EncryptationSecurityService(administrator.getPassword());
+            String password = encriptationService(administrator.getPassword());
             niconadminDAO = new NiconAdministratorDAO(administrator);
             niconadminDAO.createAdministrator();
             NiconLoginSystem initLogin = new NiconLoginSystem(NiconAdministrator.GetDataAdmin());
@@ -73,7 +77,7 @@ public class NiconSystemAdmin {
             niconadminDAO=null;
             administrator=null;
         } catch (Exception e) {
-            System.out.println(NiconSystemAdmin.GetInstantTime()+"\nOcurrio un error en NiconSystemAdmin.ActiveNiconPersonal():\n");
+            System.out.println(getInstantTime()+"\nOcurrio un error en NiconSystemAdmin.ActiveNiconPersonal():\n");
             e.printStackTrace();
         }
     }
@@ -93,70 +97,82 @@ public class NiconSystemAdmin {
      */
     
     public static boolean verifyCredentialsUser(String InputPassword) {
-        boolean acces = false;
+        
         try {
-            String encriptPass = EncryptationSecurityService(InputPassword);
+            String encriptPass = encriptationService(InputPassword);
             sentence = "select Password from administrador;";
             Data = NiConection.RestoreDataSentence(sentence);
             Data.next();
             String adminPass = Data.getString("Password");
             Data.close();
             NiConection.DisconectDB();
-            if ((adminPass.equals(encriptPass))) {
-                acces = true;
-                sentence = null;
-                encriptPass = null;
-                InputPassword = null;
-                adminPass = null;
-            }
+            
+                if ((adminPass.equals(encriptPass))) {
+                    stateOP = true;
+                    sentence = null;
+                    encriptPass = null;
+                    InputPassword = null;
+                    adminPass = null;
+                }
         } catch (Exception e) {
             System.out.println("Ocurrio un error en SecurityControlSystem()\n" + e);
         }
-        return acces;
+        return stateOP;
     }
 
     /**
+     * este metodo nos permite obtener un instante de tiempo formatearlo y convertirlo en 
+     * un String que podra ser usado por la aplicacion.
      * 
-     * @return
+     * @return String date
+     * @Author Frederick Adolfo Salazar Sanchez
      */
-    public static String GetInstantTime() {
-        String NowTime = "";
-        format = new SimpleDateFormat("dd/MM/yyyy/HH:mm");
+    
+    public static String getInstantTime() {
+        
         try {
+            format = new SimpleDateFormat("dd/MM/yyyy/HH:mm");
             date = new Date();
-            NowTime = format.format(date);
+            dateFormated = format.format(date);
         } catch (Exception e) {
             System.out.println("Ocurrio un ERROR en SystemAdmin.GetInstantTime() detail: \n" + e);
         }
-        return NowTime;
+        return dateFormated;
     }
 
     /**
+     * formatea un momento de tiempo en forma simple solo dia, mes y hora, retorna un String 
+     * con el valor obtenido y podra ser accedido desde cualquier lugar del sstema, recibe como
+     * parametro el objeto Date a dar formato
      * 
-     * @param ToFormat
-     * @return
+     * @param Dat ToFormat
+     * @return String dateFormat
      */
     public static String dateFormatSimple(Date ToFormat) {
-        String SingleDate = "";
+        
         if (ToFormat == null) {
-            SingleDate = "No se ha ingresado la fecha";
+            dateFormated = "el Objeto Date recibido no es valido valor=null";
         } else {
             try {
                 format = new SimpleDateFormat("dd/MM/yyyy");
-                SingleDate = format.format(ToFormat);
+                dateFormated = format.format(ToFormat);
             } catch (Exception e) {
+                System.out.println("Ocurrio un error en NiconSystemAdmin.dateFormatSimple():\n");
+                e.printStackTrace();
             }
         }
-
-        return SingleDate;
+        return dateFormated;
     }
 
     /**
+     * Este metodo ofrece el servicio de encriptacion de un cadena de String, se usa para
+     * codificar la contraseña del usuario que ingresa al sistema. recibe como parametro
+     * el String a encrptar y retorna un String con el valor en MD5 del proceso.
      * 
-     * @param Input
-     * @return
+     * @param String Input
+     * @return String output
      */
-    public static String EncryptationSecurityService(String Input) {
+    public static String encriptationService(String Input) {
         try {
             MessageDigest md = MessageDigest.getInstance("MD5");
             byte[] bytes = md.digest(Input.getBytes());
@@ -174,8 +190,11 @@ public class NiconSystemAdmin {
     }
 
     /**
+     * Este metodo tiene com finalidad obtener el tamaño de la pantalla sobre la que se esta
+     * trabajando. retorna un vector con los datos recibidos.
      * 
-     * @return
+     * @return int [] escrenDimension
+     * @Author Frderick Adolfo Salazar Sanchez
      */
     public static int[] DimensionScreen() {
         int ValueDimension[] = new int[2];
@@ -188,9 +207,13 @@ public class NiconSystemAdmin {
     }
 
     /**
+     * este metodo se integro el 1 de diciembre de 2012, es simple su uso solo verifica si
+     * el pc del usuario tiene o no acceso a Internet. esto se hace con la finalidad de 
+     * poder usar NiconPersonal en su modulo NiconMail y NiconTwiit de forma offline.
      * 
-     * @return
+     * @return boolean stateOP
      */
+    
     public static boolean verifyInternetConection() {
         try {
             String url = "www.twitter.com";
