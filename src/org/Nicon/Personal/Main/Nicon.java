2e76590e4d62b5db753cc210b2e2269e563deb6c
frederick <frederick@frederick-Aspire-5536>
2013-03-07 16:19:04 -0500

    Documetacion y Limpieza de codigo NiconPersonal Main

--- src/org/Nicon/Personal/Main/Nicon.java
+++ src/org/Nicon/Personal/Main/Nicon.java
@@ -1,8 +1,18 @@
+/**
+ * CopyRigth (C) 2013 NiconSystem Incorporated. 
+ * 
+ * NiconSystem Inc.
+ * Cll 9a#6a-09 Florida Valle del cauca Colombia
+ * 318 437 4382
+ * fredefass01@gmail.com
+ * desarrollador-mantenedor: Frederick Adolfo Salazar Sanchez.
+ */
 
 package org.Nicon.Personal.Main;
 
 import com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel;
 import javax.swing.UIManager;
+
 import org.Nicon.Personal.GuiUser.Nicon.GuiActivation;
 import org.Nicon.Personal.GuiUser.Nicon.NiconLoginSystem;
 import org.Nicon.Personal.LibCore.Sbin.GlobalConfigSystem;
@@ -14,8 +24,8 @@ import org.Nicon.Personal.LibCore.Sbin.NiconUpdate;
 
 /**
  * Esta es la clase de inicio de la aplicacion, en ella damos los primeros pasos cuando es ejecutada
- * la suite de informacion. en primer medida lo que hacemos es inicializar todas las configuraciones
- * del sistema. luego se procede a ajustar el look and feel Nimbus que sera el sistema por defecto
+ * la suite de informacion en primer medida lo que hacemos es inicializar todas las configuraciones
+ * del sistema luego se procede a ajustar el look and feel Nimbus que sera el sistema por defecto
  * posterior a ello procedemos a validar el estado del sistema que solo pueden ser dos o activo o inactivo
  * esto lo hacemos mediante la interfaz NiconSystemAdmin que es el sistema de adminsistracion central de la 
  * suite. en caso de NO estar activada se procede a ello o de lo contrario entramos a validar
@@ -34,17 +44,14 @@ public class Nicon{
       
                     try{
                         GlobalConfigSystem config=new GlobalConfigSystem();
-                        System.out.println("Iniciando "+GlobalConfigSystem.getTitleAplication());
                         UIManager.setLookAndFeel(new NimbusLookAndFeel());
                         boolean estado = NiconSystemAdmin.verifyStateSystem();
-                        System.out.println("Verificando estado del sistema ..."); 
                             if (!estado) {
                                    System.out.println("Iniciando activación de "+GlobalConfigSystem.getTitleAplication());
                                    GuiActivation activar = new GuiActivation();
                                    activar.setLocationRelativeTo(null);
                                    activar.setVisible(true);                                   
                            } else {
-                                  System.out.println("Iniciando componentes de "+GlobalConfigSystem.getTitleAplication());
                                   NiconUpdate.VerifySystemVersion();
                                   NiconLoginSystem Login = new NiconLoginSystem(NiconAdministrator.GetDataAdmin());
                                   Login.setVisible(true);
